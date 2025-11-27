e79d7818af330e8c5dd2bd1f616f2b0ebe269df7
bigsmokeDev <msoheib2010@gmail.com>
2025-11-27 21:03:07 +0100

    fix build.bat (needs testing)

--- build.bat
+++ build.bat
@@ -1,11 +1,19 @@
+:: this build.bat uses MinGW64
 @echo off
 
-:: this build.bat uses MinGW64
+IF "%1"=="" (
+	echo "usage: build.bat [RELEASE]"
+	exit /b
+)
 
-set CFLAGS = -std=c99 -Wall -Iwindbolt
-set LDFLAGS = -luser32 -ld3d11 -ldxgi -ld3dcompiler
+set CFLAGS=-std=c99 -Wall -Iwindbolt
+set LDFLAGS=-luser32 -ld3d11 -ldxgi -ld3dcompiler
 
-set DEBUG_FLAGS = -O0 -g
-set RELEASE_FLAGS = -O3 -DNDEBUG
+set DEBUG_FLAGS=-O0 -g
+set RELEASE_FLAGS=-O3 -DNDEBUG
 
-gcc windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %DEBUG_FLAGS%
+IF "%1"=="RELEASE" (
+	gcc windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %RELEASE_FLAGS%
+) ELSE (
+	gcc windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %DEBUG_FLAGS%
+)
