e441526824086079a6b79d69a5a249184aebb75d
bigsmokeDev <msoheib2010@gmail.com>
2025-12-11 22:51:14 +0100

    temp build.bat

--- build.bat
+++ build.bat
@@ -7,8 +7,11 @@ set LDFLAGS=-luser32 -lgdi32 -lopengl32
 set DEBUG_FLAGS=-O0 -g
 set RELEASE_FLAGS=-O3 -DNDEBUG
 
+:: TODO(smoke): check if vendor.o exists
+gcc -c windbolt/vendor/build.c -o bin/vendor.o -O3
+
 IF "%1"=="RELEASE" (
-	gcc windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %RELEASE_FLAGS%
+	gcc bin/vendor.o windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %RELEASE_FLAGS%
 ) ELSE (
-	gcc windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %DEBUG_FLAGS%
+	gcc bin/vendor.o windbolt/build.c game/main.c -o bin/out.exe %CFLAGS% %LDFLAGS% %DEBUG_FLAGS%
 )
